88dfbab839dbdf5f6ffcde5af4f7795b94a7de9f
minji_105 <ajhjmj9@naver.com>
2025-06-07 20:46:14 +0900

    ✨ Feat: 각 페이지 라우팅 구성
    메인(/), 로그인(/login), 회원가입(/register), 보관함(/keep) 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Saved from './pages/Saved';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
