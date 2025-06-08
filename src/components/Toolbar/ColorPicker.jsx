e2ce2a4cb7e50da04b32f96310a69293abc03b00
minji_105 <ajhjmj9@naver.com>
2025-06-09 00:55:52 +0900

    Revert "✨ Feat: color 전역 상태 생성"
    This reverts commit 43037643e6a85cf2efe91267e41f9999174ca377.

--- src/components/Toolbar/ColorPicker.jsx
+++
@@ -1,8 +0,0 @@
-import { useColor } from '../../context/ColorContext';
-
-function ColorPicker() {
-  const { color } = useColor();
-  return <div>{color}</div>;
-}
-
-export default ColorPicker;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { ColorProvider } from './context/ColorContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ColorProvider>
-      <App />
-    </ColorProvider>
+    <App />
   </StrictMode>,
 );
